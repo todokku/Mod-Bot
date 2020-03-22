e29ed6cd06880fcdefb7981452d6d950ac61673a
qalue <55650376+qalue@users.noreply.github.com>
2020-03-22 09:50:27 +0000

    Add files via upload

---
+++ commands/info/whois.js
@@ -0,0 +1,10 @@
+module.exports = {
+    name: "whois",
+    aliases: ["userinfo", "user", "who"],
+    category: "Info",
+    description: "Returns user information",
+    usage: "[username | id, | mention",
+    run: async (client, message, args) => {
+        
+    }
+}
---
+++ commands/moderation/say.js
@@ -0,0 +1,27 @@
+module.exports = {
+    name: "say",
+    aliases: ["bc", "broadcast"],
+    category: "moderation",
+    description: "Says your input via the bot",
+    usage: "<input>",
+    run: async (client, message, args) => {
+        const { Client, MessageEmbed, Collection} = require("discord.js");
+        
+        if (message.deletable) message.delete();
+
+        if (args.length < 1)
+            return message.reply("Nothing to say?").then(m => m.delete(5000));
+
+        const roleColor = message.guild.me.displayHexColor === "#000000" ? "#ffffff" : message.guild.me.displayHexColor;
+
+            const embed = new MessageEmbed()
+            .setTitle("New Message!")
+            .setColor(roleColor)
+            .setDescription(args.join(" "))
+            .setTimestamp()
+            .setAuthor(message.author.username, message.author.displayAvatarURL)
+            .setFooter(client.user.username, client.user.displayAvatarURL);
+            
+            message.channel.send(embed);
+    }
+}
