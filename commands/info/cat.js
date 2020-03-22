a93037d28dd7239c1ba104c0d11fa88c86921211
qalue <55650376+qalue@users.noreply.github.com>
2020-03-22 12:06:12 +0000

    Add files via upload

---
+++ commands/info/cat.js
@@ -0,0 +1,26 @@
+module.exports = {
+    name: "cat",
+    aliases: ["kitten", "kitty"],
+    category: "info",
+    description: "Shows a cute picture of a cat",
+    run: async (client, message, args) => {
+
+        let msg = await message.channel.send("Looking for kitty...")
+
+        let {body} = await superagent
+        .get(`http://aws.random.cat/meow`)
+        //console.log(body.file)
+        if(!{body}) return message.channel.send("I broke! Try again.")
+
+            let cEmbed = new Discord.RichEmbed()
+            .setColor(colours.cyan)
+            .setAuthor(`ModBot CATS!`, message.guild.iconURL)
+            .setImage(body.file)
+            .setTimestamp()
+            .setFooter(`ModBot`, bot.user.displayAvatarURL)
+
+            message.channel.send({embed: cEmbed});
+
+            msg.delete();
+    }
+}
