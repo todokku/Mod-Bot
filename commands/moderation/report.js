eae532b1505e445aa6b9acd001f07e6451678b86
qalue <55650376+qalue@users.noreply.github.com>
2020-03-22 18:28:03 +0000

    Add files via upload

--- commands/moderation/report.js
+++ commands/moderation/report.js
@@ -12,18 +12,18 @@ module.exports = {
         let rMember = message.mentions.members.first() || message.guild.members.get(args[0]);
 
         if (!rMember)
-            return message.reply("Couldn't find that person").then(m => m.delete(5000));
+            return message.reply("Couldn't find that person").then(m => m.delete({timeout: 5000}));
 
         if (rMember.hasPermission("BAN_MEMBERS") || rMember.user.bot)
-            return message.reply("Can't report that member").then(m => m.delete(5000));
+            return message.reply("Can't report that member").then(m => m.delete({timeout: 5000}));
 
         if (!args[1])
-            return message.channel.send("Please provide a reason for the report!").then(m => m.delete(5000));
+            return message.channel.send("Please provide a reason for the report!").then(m => m.delete({timeout: 5000}));
 
         const channel = message.guild.channels.find(channel => channel.name === "reports");
 
         if (!channel)
-            return message.channel.send("I could not find a \`#reports`\ channel").then(m => m.delete(5000));
+            return message.channel.send("I could not find a \`#reports`\ channel").then(m => m.delete({timeout: 5000}));
 
         const embed = new MessageEmbed()
             .setColor("#ff0000")
