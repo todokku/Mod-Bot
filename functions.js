e919d85e9d771718c441a8539dc5ea7236edbf0a
qalue <55650376+qalue@users.noreply.github.com>
2020-03-22 10:50:56 +0000

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,26 @@
+module.exports = {
+    getMember(message, toFind = '') {
+        toFind = toFind.toLowerCase();
+
+        let target = message.guild.members.get(toFind);
+
+        if (!target && message.mentions.members)
+            target = message.mentions.members.first();
+
+        if (!target && toFind) {
+            target = message.guild.members.find(member => {
+                return member.displayName.toLowerCase().includes(toFind) ||
+                member.user.tag.toLowerCase().includes(tofind)
+            });
+        }
+
+        if (!target)
+            target = message.member;
+
+        return target;
+    },
+
+    formatDate: function(date) {
+        return new Int16Array.DateTimeFormat('en-US').format(date);
+    }
+}
